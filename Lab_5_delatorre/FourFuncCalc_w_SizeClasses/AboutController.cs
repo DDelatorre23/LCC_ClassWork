8a9f37eed4d1ca4022d7bb4528471255c8847e05
DDelatorre23 <dj_delatorre23@yahoo.com>
2017-02-18 18:54:15 -0800

    Lab 5

---
+++ Lab_5_delatorre/FourFuncCalc_w_SizeClasses/AboutController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace FourFuncCalc
+{
+    public partial class AboutController : UIViewController
+    {
+        public AboutController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ Lab_5_delatorre/FourFuncCalc_w_SizeClasses/Calculations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace FourFuncCalc
+{
+	public class Calculations
+	{
+		public static double Addition(double x, double y)
+		{
+			return x + y;
+		}
+
+		public static double Subtraction(double x, double y)
+		{
+			return x - y;
+		}
+
+		public static double Division(double x, double y)
+		{
+			return x / y;
+		}
+
+		public static double Multiply(double x, double y)
+		{
+			return x * y;
+		}
+	}
+}
+
